4860f8fe0718f6b0092f79539025280bb6847109
Allie Larson <allisonlarson91@gmail.com>
2015-11-07 15:42:29 -0500

    Add spec for Expense#build_payments

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -6,6 +6,25 @@ RSpec.describe Expense do
   it { should belong_to(:society) }
   it { should have_many(:payments) }
 
+  describe "#build_payments" do
+    context "with multiple roommates" do
+      let!(:society) { Society.create!(name: "test") }
+      let!(:expense) { Expense.create!(amount_cents: 5000, user: user1, society: society)}
+      let!(:user1) { User.create!(email: "test@example.com", password: "password", society: society)}
+      let!(:user2) { User.create!(email: "test2@example.com", password: "password", society: society)}
+      let!(:user3) { User.create!(email: "test3@example.com", password: "password", society: society)}
+
+      it "builds new payments" do
+        expense.build_payments
+        expect{ expense.save! }.to change{ Payment.count }.by(2)
+        payment = Payment.last
+        expect(payment.amount_cents).to eq(1666)
+        expect(payment.payee).to eq(user1)
+        expect(payment.payer).to eq(user3)
+      end
+    end
+  end
+
   describe "#credit_user" do
     let!(:society) { Society.create!(name: "test") }
     let!(:expense) { Expense.create!(amount_cents: 7500, user: user1, society: society)}
@@ -19,3 +38,4 @@ RSpec.describe Expense do
     end
   end
 end
+
